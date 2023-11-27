df510a2dacfc6672162bac5364c015e368829971
nardyy01 <nardyy01@gmail.com>
2023-11-27 18:12:01 -0500

    Add about me section

--- portfolio_proj/app/components/AboutSection.jsx
+++ portfolio_proj/app/components/AboutSection.jsx
@@ -3,9 +3,46 @@ import React, { useTransition, useState } from 'react'
 import Image from 'next/image'
 import TabButton from './TabButton'
 
+const TAB_DATA = [
+    {
+        title: 'Skills',
+        id: 'skills',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>C++</li>
+                <li>JavaScript</li>
+                <li>Perl</li>
+                <li>Node.js</li>
+                <li>C#</li>
+                <li>DB2</li>
+                <li>Python</li>
+                <li>React</li>
+            </ul>
+        )
+    },
+    {
+        title: 'Education',
+        id: 'education',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>Clayton State University - Bachelor's Degree</li>
+            </ul>
+        )
+    },
+    {
+        title: 'Certifications',
+        id: 'certifications',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>JavaScript Algorithms and Data Structures</li>
+            </ul>
+        )
+    }
+]
+
 const AboutSection = () => {
     const [tab, setTab] = useState("skills")
-    const [startTransition, isPending] = useTransition();
+    const [isPending, startTransition] = useTransition();
 
     const handleTabChange = (id) => {
         startTransition(() => {
@@ -22,19 +59,19 @@ const AboutSection = () => {
                     alt='Mood Image'
                     width={500}
                     height={500} />
-                <div>
+                <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                     <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
                     <p>
                         Things about me...
                     </p>
-                    {/*  */}
+                    {/* Tab buttons for select information */}
                     <div className='flex flex-row mt-8'>
-                        <TabButton selectTab={() => handleTabChange('skills')} active={tab === 'skills'}></TabButton>
-                        <TabButton selectTab={() => handleTabChange('skills')} active={tab === 'skills'}></TabButton>
-                        <TabButton selectTab={() => handleTabChange('skills')} active={tab === 'skills'}></TabButton>
-                        <span>Skills</span>
-                        <span>Education</span>
-                        <span>Experience</span>
+                        <TabButton selectTab={() => handleTabChange('skills')} active={tab === 'skills'}>Skills</TabButton>
+                        <TabButton selectTab={() => handleTabChange('education')} active={tab === 'education'}>Education</TabButton>
+                        <TabButton selectTab={() => handleTabChange('certifications')} active={tab === 'certifications'}>Certifications</TabButton>
+                    </div>
+                    <div className='mt-8 ml-3'>
+                        {TAB_DATA.find((tabCurr) => tabCurr.id === tab).content}
                     </div>
                 </div>
             </div>
